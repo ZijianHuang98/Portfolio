637dec20f0b367ac5df411edbdbe807bc2a74a93
Zijian Huang <jessehuang05@yahoo.com>
2021-12-22 09:46:55 -0600

    Working on top bar, add some icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,8 +1,26 @@
 import "./topbar.scss"
+import EmailIcon from '@mui/icons-material/Email';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 
 export default function Topbar() {
     return (
         <div className="topbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">Zijian</a>
+                    <div className="itemContainer">
+                        <InsertEmotiIcon className="icon" />
+                        <span>+1 314 532 0668</span>
+                    </div>
+                    <div className="itemContainer">
+                        <EmailIcon className="icon" />
+                        <span>jessehuang05@yahoo.com</span>
+                    </div>
+                </div>
+                <div className="right">
+
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -2,7 +2,7 @@ import "./works.scss"
 
 export default function Work() {
     return (
-        <div className="works">
+        <div className="works" id = "works">
         </div>
     )
 }
