5d8ff594aea49a79f1d14d3ba8785f360a303dd7
Zijian Huang <jessehuang05@yahoo.com>
2021-12-23 14:39:03 -0600

    add some animation on top bar

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
+import React, { useEffect, useState } from "react";
+
 import "./app.scss";
 
 function App() {
+  const[menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="App">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro />
         <Portfolio />
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,24 +1,27 @@
 import "./topbar.scss"
-import EmailIcon from '@mui/icons-material/Email';
-import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
+import {EmojiEmotions,Email} from "@material-ui/icons"
 
-export default function Topbar() {
+export default function Topbar({menuOpen,setMenuOpen}) {
     return (
-        <div className="topbar">
+        <div className={"topbar " + (menuOpen && "active")}>
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">Zijian</a>
                     <div className="itemContainer">
-                        <InsertEmotiIcon className="icon" />
+                        < EmojiEmotions className="icon" />
                         <span>+1 314 532 0668</span>
                     </div>
                     <div className="itemContainer">
-                        <EmailIcon className="icon" />
+                        <Email className="icon" />
                         <span>jessehuang05@yahoo.com</span>
                     </div>
                 </div>
                 <div className="right">
-
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
                 </div>
             </div>
         </div>
