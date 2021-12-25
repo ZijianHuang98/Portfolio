763ac2536db8574b8d3e928baafae0ab53695f07
Zijian Huang <jessehuang05@yahoo.com>
2021-12-24 20:38:51 -0600

    finish the menu and some of intro

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import Contact from "./components/contact/Contact";
 import React, { useEffect, useState } from "react";
 
 import "./app.scss";
+import Menu from "./components/menu/Menu";
 
 function App() {
   const[menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="App">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro />
         <Portfolio />
--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -2,7 +2,20 @@ import "./intro.scss"
 
 export default function intro() {
     return (
-        <div className="intro" id = "intro">
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/park.png" />
+                </div>
+            </div>
+            <div className="right">
+            <div className="wrapper">
+                <h2>Hi there, I'm</h2>
+                <h1>Zijian Huang</h1>
+                <h3>Junior Software Engineer<span></span></h3>
+            </div>
+            
+            </div>
         </div>
     )
 }
