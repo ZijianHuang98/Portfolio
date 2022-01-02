1773c692cba9a459092f9709b201b8dc70176cc4
Zijian Huang <jessehuang05@yahoo.com>
2022-01-01 22:57:06 -0600

    part of portfolio

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -3,6 +3,52 @@ import "./portfolio.scss"
 export default function portfolio() {
     return (
         <div className="portfolio" id = "portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Feature</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Desing</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/p1.jpeg" alt="" />
+                    <h3>
+                        Banking App
+                    </h3>
+                </div>
+                <div className="item">
+                    <img src="assets/p1.jpeg" alt="" />
+                    <h3>
+                        Banking App
+                    </h3>
+                </div>
+                <div className="item">
+                    <img src="assets/p1.jpeg" alt="" />
+                    <h3>
+                        Banking App
+                    </h3>
+                </div>
+                <div className="item">
+                    <img src="assets/p1.jpeg" alt="" />
+                    <h3>
+                        Banking App
+                    </h3>
+                </div>
+                <div className="item">
+                    <img src="assets/p1.jpeg" alt="" />
+                    <h3>
+                        Banking App
+                    </h3>
+                </div>
+                <div className="item">
+                    <img src="assets/p1.jpeg" alt="" />
+                    <h3>
+                        Banking App
+                    </h3>
+                </div>
+            </div>
         </div>
     )
 }
