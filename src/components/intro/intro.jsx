b333144464ddc17d20822dab3a66585a642b2b97
Zijian Huang <jessehuang05@yahoo.com>
2022-01-01 19:10:43 -0600

    Finished the intro part

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,20 +1,37 @@
 import "./intro.scss"
+import { init } from 'ityped';
+import { useEffect, useRef } from 'react';
 
-export default function intro() {
+export default function Intro() {
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backDelay:  1500,
+            backSpeed:60,
+
+            strings: ["Front-end Developer", "Fishing Expert","Badminton Athlete"],
+        });
+    }, []);
+
+
     return (
         <div className="intro" id="intro">
             <div className="left">
                 <div className="imgContainer">
-                    <img src="assets/park.png" />
+                    <img src="assets/z1.png" />
                 </div>
             </div>
             <div className="right">
-            <div className="wrapper">
-                <h2>Hi there, I'm</h2>
-                <h1>Zijian Huang</h1>
-                <h3>Junior Software Engineer<span></span></h3>
-            </div>
-            
+                <div className="wrapper">
+                    <h2>Hi there, I'm</h2>
+                    <h1>Zijian Huang</h1>
+                    <h3>Full-time  <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
             </div>
         </div>
     )
